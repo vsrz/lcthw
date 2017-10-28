e55fb45da472059f22c2008cec47f1fa91bdceb2
vsrz <ritual@vsritual.com>
2017-10-27 21:59:33 -0700

    Updates

--- ex26/logfind.c
+++ ex26/logfind.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include "dbg.h"
 
 const int MAX_FILE_SEARCHES = 8;
@@ -20,12 +21,30 @@ logfile* loadfile(char* filename)
     return l;
 }
 
+int readfile(logfile *l)
+{
+    l->fp = fopen(l->path, "r");
+    if (l->fp == 0) {
+        return 0;
+    } else { 
+        printf("Error %d encountered while loading file %s", errno, l->path);
+    }
+    return 1;
+}
+
 int main(int argc, char **argv)
 {
     char* filename = "filetest.txt";
+    char* needle = "findme";
     logfile *log;
     log = loadfile(filename);
-    printf("%s", log->path);
+
+    if (readfile(log) > 0) {
+        printf("String %s was matched in %s\n", needle, log->path);
+    } else {
+        printf("%s was not found in %s\n", needle, log->path);
+    }
+
     return 0;
 }
 
