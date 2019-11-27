736b0938ec34a5135dc7a685e6e61a4335b03b7f
Jeremy Villegas <jeremy.villegas@servicenow.com>
2019-11-26 19:39:28 -0800

    Updates

---
+++ ex4/ex.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int age;// = 10;
+    int height = 72;
+    char *t = NULL;
+    printf("%c", t[0]);
+
+    // break here
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
+
+}
+
---
+++ ex9/ex.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    while (i < 25) {
+        printf("%d", i++);
+    }
+
+    // need this to add a final newline
+    printf("\n");
+
+    return 0;
+}
